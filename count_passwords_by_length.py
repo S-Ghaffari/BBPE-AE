175065ee3e0cae4393a0e0c6b874ca2e3365f889
S-Ghaffari <Samane_ghafari@znu.ac.ir>
2024-06-06 20:11:47 +0330

    count_passwords_by_length.py

--- count_passwords_by_length.py
+++ count_passwords_by_length.py
@@ -1,28 +1,22 @@
 def count_passwords_by_length(passwords):
     password_length_count = {}
-    
-    for password in passwords:
+        for password in passwords:
         length = len(password)
         if length in password_length_count:
             password_length_count[length] += 1
         else:
             password_length_count[length] = 1
-    
-    return password_length_count
+        return password_length_count
 
-# Read passwords from a file
-file_path = r'D:\smne\final - Copy\myspace\train_myspace.txt'  
+file_path = 'train_myspace.txt'  
 passwords = []
 
 with open(file_path, 'r') as file:
     for line in file:
-        password = line.rstrip('\n')  # Remove trailing newline character
+        password = line.rstrip('\n') 
         passwords.append(password)
 
-# Count passwords by length
 password_length_count = count_passwords_by_length(passwords)
-
-# Print the summary in ascending order
 for length in sorted(password_length_count.keys()):
     count = password_length_count[length]
     print(f"Number of {length}-character passwords: {count}")
