f52e540cb00bbfd3f7d2822b386c147ac0dba38c
S-Ghaffari <Samane_ghafari@znu.ac.ir>
2024-06-06 20:13:20 +0330

    bleu.py

--- bleu.py
+++ bleu.py
@@ -5,27 +5,21 @@ def GetBLEU(Passwords0:list[str], References0:list[str]) -> float:
     print(len(Passwords0))
     print(len(References0))
     
-    Passwords = [[i for i in Password0] for Password0 in Passwords0]        #['m', 'i', 'l', 'u', 'p', 'e']
-    #print((Passwords))                                                         #<class 'list'>
+    Passwords = [[i for i in Password0] for Password0 in Passwords0]        
     References = [[i for i in References0] for References0 in References0]
-    #print("*"*60)
-    #print(type(References))
-    
+
     mBLEUs = []
     for Password in Passwords:
         ts = []
         for Reference in References:
             t = bleu.sentence_bleu(references=[Reference],
                                    hypothesis=Password,
-                                   weights=[0.25])
+                                   weights=[0.25])        # or 0.5
             ts.append(t)
         mBLEU = np.max(ts)
         mBLEUs.append(mBLEU)
     amBLEU = np.mean(mBLEUs)
     return amBLEU
-
-
-
 
 with open(file='test_hotmail.txt', mode='r', encoding='UTF-8',errors='ignore') as F:
     Passwords = F.read().split('\n')
